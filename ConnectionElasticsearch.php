fd2b40edccc9d766e95d0993f5c05fce98d4f7ce
sina1010anis <sina1010anis@gmail.com>
2023-09-24 20:04:35 +0330

    update 1

--- ConnectionElasticsearch.php
+++ ConnectionElasticsearch.php
@@ -6,10 +6,13 @@ use Elastic\Elasticsearch\ClientBuilder;
 class ConnectionElasticsearch
 {
     protected $connetion_data;
+    protected $where_data;
+
 
     public function __construct()
     {
 
+        // Connect to elasticsearch using two variables ELASTICSEARCH_USERNAME and ELASTICSEARCH_PASSWORD
         $this->connetion_data = ClientBuilder::create()
             ->setBasicAuthentication(env('ELASTICSEARCH_USERNAME'), env('ELASTICSEARCH_PASSWORD'));
 
@@ -17,7 +20,7 @@ class ConnectionElasticsearch
 
     public function connction()
     {
-
+        // Check the connection
         return $this->connetion_data;
 
     }
@@ -32,12 +35,15 @@ class ConnectionElasticsearch
     public function connctionVersion()
     {
 
+        // Check the version
         return $this->connctionBuild()->info()['version']['number'];
 
     }
 
     public function connctionIndex(array $data)
     {
+
+        //Save to elasticsearch
 
         // $data exampel =>
             // $params = [
@@ -53,9 +59,25 @@ class ConnectionElasticsearch
 
     }
 
-    public function connctionSearch(array $data, bool $show_only_data = false)
+    public function connectionWhere(array $wheres = [], string $index = '_all')
+    {
+        $push_where = [];
+        foreach ($wheres as $where) {
+            $push_where['match'] = $where;
+        }
+        $this->where_data = ($where == []) ? ['index' => $index] : [
+            'index' => $index,
+            'body' => [
+                'query' => $push_where
+            ]
+        ];
+        return $this;
+    }
+    public function connctionSearch(bool $show_only_data = false)
     {
 
+        //Select to elasticsearch
+        $data_only = [];
         // $data exampel =>
             // $params = [
             //     'index' => 'my_test',
@@ -68,9 +90,12 @@ class ConnectionElasticsearch
             //     ]
             // ];
 
-        $res = $this->connctionBuild()->search($data);
+        $res = $this->connctionBuild()->search($this->where_data);
         if ($show_only_data) {
-            return $res['hits']['hits'];
+            foreach ($res['hits']['hits'] as $source) {
+                $data_only[] = $source['_source'];
+            }
+            return $data_only;
         }
         return $res;
 
